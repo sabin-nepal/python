a7a68aea055ba56ad234d9e7ea3d813072cde620
Sabin <sabinnepal2k17@gmail.com>
2019-02-02 08:44:28 +0545

    day 3

---
+++ marks.py
@@ -0,0 +1,30 @@
+# marks = int(input("Enter Marks"))
+# if marks>=90 and marks<=100:
+#     print("You are in grade A")
+# elif marks>=70 and marks<90:
+#     print("You are in grade B")
+# elif marks>=40 and marks<70:
+#     print("You are in grade C")
+# else:
+#     print("you are failed")
+
+# number =  input("Enter a name")
+# if number.isalpha() and len(number) > 2 :
+#     print("valid")
+# else:
+#     print("invalid")
+
+#print("1\n2\n")
+
+name = input("Enter Your name")
+while name.isalpha()==True:
+    print("Wrong input")
+    name = input("Enter Your name")
+    print(name)
+
+# year = int(input("Enter your birth year"))
+# age = 2019-year
+# if age>=18:
+#     print("You are eligible")
+# else:
+#     print("You are noit");
